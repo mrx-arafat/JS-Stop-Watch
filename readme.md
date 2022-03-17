f16d0aaa0f8ae3bed5b3b87461f6fb473edf5da7
Easin Arafat <arafatmrx@gmail.com>
2022-03-17 21:16:46 +0600

    ms done

--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 ## Lessons learned from here
 
 - document.querySelector(".timer") >>> ekhane dot dite vule jai >>> but document.getElementById("start")>>> eita te ei jahamela nai
+
+- setInterval(run,10) >>> run namer fn ta 10ms por por run hobe
--- script.js
+++ script.js
@@ -7,3 +7,22 @@ let counter;
 const start = document.getElementById("start");
 const stop = document.getElementById("stop");
 const reset = document.getElementById("reset");
+
+let time = document.querySelector(".time");
+
+// start btn
+
+start.addEventListener("click", function () {
+  //setInterval ei fn er vitore dei nai first e
+
+  counter = setInterval(run, 10);
+  function run() {
+    time.textContent = m + ":" + s + ":" + ms;
+    ms++;
+
+    if (ms == 100) {
+      ms = 0;
+      s++;
+    }
+  }
+});
